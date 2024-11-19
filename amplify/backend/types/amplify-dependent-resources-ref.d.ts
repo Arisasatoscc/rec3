3378536fb509a808cd2d177df196ca3a684f0341
Arisasatoscc <sarisa@sisco-consulting.co.jp>
2024-11-19 16:23:32 +0900

    backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "rec3": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "rec3": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState();
+
+  const testreqest = fetch("https://op58ng3qyh.execute-api.ap-northeast-1.amazonaws.com/default/")
+    .then((res) => res.json()) // JSON形式に変換
+    .then((json) => setData(json))
+    .catch(() => alert("error")); // エラー発生時にアラート
+
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">
